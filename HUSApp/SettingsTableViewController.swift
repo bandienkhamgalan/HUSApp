de50cd2eb15aae93c42d479ccaecb7352444bd04
donovanOng <dononvan.ongyl@gmail.com>
2015-03-14 00:46:31 +0000

    - Add link and unlink with Dropbox in Settings

--- HUSApp/SettingsTableViewController.swift
+++ HUSApp/SettingsTableViewController.swift
@@ -16,12 +16,18 @@ class SettingsTableViewController: UITableViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    @IBOutlet var infoDropbox: UITableViewCell!
+    @IBOutlet var actionDropbox: UITableViewCell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         var doneButton : UIBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: "done")
         doneButton.tintColor = themeColour
         self.navigationItem.rightBarButtonItem = doneButton
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: themeColour]
+        
+        updateView()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,61 +35,27 @@ class SettingsTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    // MARK: - Table view data source
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
-        // Configure the cell...
-
-        return cell
+    func updateView(){
+        if DBAccountManager.sharedManager().linkedAccount != nil {
+            actionDropbox.textLabel?.text = "Sign Out from Dropbox"
+            actionDropbox.textLabel?.textColor = UIColor.redColor()
+            infoDropbox.detailTextLabel?.text = DBAccountManager.sharedManager().linkedAccount.info.userName
+            
+        } else {
+            actionDropbox.textLabel?.text = "Link with Dropbox"
+            actionDropbox.textLabel?.textColor = themeColour
+            infoDropbox.detailTextLabel?.text = "Not Available"
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 1 && DBAccountManager.sharedManager().linkedAccount != nil {
+            DBAccountManager.sharedManager().linkedAccount.unlink()
+        } else if indexPath.section == 1 {
+            DBAccountManager.sharedManager().linkFromController(self)
+        }
+        
+        updateView()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
